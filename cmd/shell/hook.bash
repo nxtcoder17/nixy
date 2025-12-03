b3baff00286aa6f32be14b2c49911cf8e146bc66
nxtcoder17 <nxtcoder17@gmail.com>
2025-12-03 13:31:46 +0530

    feat: updates bash shell hook

--- cmd/shell/hook.bash
+++ cmd/shell/hook.bash
@@ -1,43 +1,27 @@
 NIXY_LAST_DIR=""
 
+__nixy_debug() {
+  echo "[## NIXY DEBUG] $@"
+}
+
 __nixy_shell_hook() {
   if [[ -z "$NIXY_SHELL" ]] && [[ ! -f nixy.yml ]]; then
     __ps1_cleanup
     return
   fi
 
-  if [[ -n "$NIXY_SHELL" ]]; then
-    __ps1_add_prefix
+  if [[ -n "$NIXY_SHELL" ]] && [[ -z "$NIXY_PROMPT_PREFIX" ]]; then
+    local cyan dim_cyan color_reset
+    cyan="\[\033[0;36m\]"
+    dim_cyan="\[\033[2;36m\]"
+    color_reset="\[\033[0m\]"
+
+    NIXY_PROMPT_PREFIX="${NIXY_PROMPT_PREFIX:-${dim_cyan}[${cyan} ᵧ${dim_cyan}]${color_reset}}"
   fi
 
   if [[ -z "$NIXY_SHELL" ]] && [[ "$NIXY_LAST_DIR" != "$PWD" ]]; then
     NIXY_LAST_DIR="$PWD"
     nixy shell
-  fi
-}
-
-__ps1_add_prefix() {
-  if [[ -z "$__nixy_premod_PS1" ]]; then
-    __nixy_premod_PS1="$PS1"
-  fi
-
-
-  local cyan dim_cyan color_reset
-  cyan="\[\033[0;36m\]"
-  dim_cyan="\[\033[2;36m\]"
-  color_reset="\[\033[0m\]"
-
-  NIXY_PROMPT_PREFIX="${NIXY_PROMPT_PREFIX:-${dim_cyan}[${cyan} ᵧ${dim_cyan}]${color_reset} }"
-
-  # prevent double prefix
-  if [[ "$PS1" != "${NIXY_PROMPT_PREFIX}"* ]]; then
-    PS1="${NIXY_PROMPT_PREFIX}${__nixy_premod_PS1}"
-  fi
-}
-
-__ps1_cleanup() {
-  if [ -n "$__nixy_premod_PS1" ]; then
-    PS1=$__nixy_premod_PS1
   fi
 }
 
